39fcacb99956b97b406bdd5ed7297a457f60d10a
mahdi1234 <mahdi1234@centrum.cz>
2021-01-27 18:31:54 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,1 @@
-wget -O mlekomaty.json  'http://overpass-api.de/api/interpreter?data=%5Bout%3Ajson%5D%5Btimeout%3A100%5D%3Barea%283600051684%29%2D%3E%2EsearchArea%3B%28node%5B%22amenity%22%3D%22vending%5Fmachine%22%5D%5B%22vending%22%7E%22milk%22%5D%28area%2EsearchArea%29%3Bway%5B%22amenity%22%3D%22vending%5Fmachine%22%5D%5B%22vending%22%7E%22milk%22%5D%28area%2EsearchArea%29%3Brelation%5B%22amenity%22%3D%22vending%5Fmachine%22%5D%5B%22vending%22%7E%22milk%22%5D%28area%2EsearchArea%29%3B%29%3Bout%3B%3E%3Bout%20skel%20qt%3B%0A'
-
+curl --help
