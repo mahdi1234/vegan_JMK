4c0dde2ce870b2ce465c04c98652d66314dcf9ef
befeleme <w_oczekiwaniu@interia.pl>
2021-02-19 17:38:17 +0100

    Lets process data better with Python

--- processing.py
+++ processing.py
@@ -1,62 +1,59 @@
 import json
-
-with open("source_data.json") as f:
-    whole_data = json.loads(f.read())
+import os
 
 
-restaurants = whole_data["elements"]
-tags = "tags"
-vegan_key = "diet:vegan"
-vegetarian_key = "diet:vegetarian"
+TAGS = "tags"
+VEGAN_KEY = "diet:vegan"
+VEGETARIAN_KEY = "diet:vegetarian"
 
 pure_vegan = []
 pure_vegetarian_with_vegan_options = []
 pure_vegetarian = []
 mixed = []
-errors = []
+not_sorted = []
 
+RESTAURANTS_WITH_NAMES = {
+    "only_vegan": pure_vegan,
+    "only_vegetarian": pure_vegetarian,
+    "vegetarian_with_vegan_options": pure_vegetarian_with_vegan_options,
+    "some_veg_options": mixed,
+    "not_sorted": not_sorted,
+}
+
+
+with open("./output/source_data.json") as f:
+    source = json.loads(f.read())
+
+restaurants = source["elements"]
 
 for restaurant in restaurants:
-    # In some cases the restaurant is added as a "type": "way" instead of "node"
-    # Then each node of the way doesn't have separate tags
-    if restaurant.get(tags):
-        vegan_present = restaurant[tags].get(vegan_key)
-        vegetarian_present = restaurant[tags].get(vegetarian_key)
+    # In some cases a node can contain no tags, skip the record then
+    if restaurant.get(TAGS):
+        vegan = restaurant[TAGS].get(VEGAN_KEY)
+        vegetarian = restaurant[TAGS].get(VEGETARIAN_KEY)
 
-        if vegan_present == "only":
+        if vegan == "only":
             pure_vegan.append(restaurant)
-        elif vegetarian_present == "only" and vegan_present == "yes":
+        elif vegetarian == "only" and vegan == "yes":
             pure_vegetarian_with_vegan_options.append(restaurant)
-        elif vegetarian_present == "only":
+        elif vegetarian == "only":
             pure_vegetarian.append(restaurant)
         elif (
-            vegetarian_present == "yes" and vegan_present == "yes"
-            or vegan_present == "yes"
-            or vegetarian_present == "yes"
+            vegetarian == "yes" and vegan == "yes"
+            or vegan == "yes"
+            or vegetarian == "yes"
         ):
             mixed.append(restaurant)
         else:
-            errors.append(restaurant)
+            not_sorted.append(restaurant)
 
 
-sorted_restaurants = [
-    pure_vegan,
-    pure_vegetarian,
-    pure_vegetarian_with_vegan_options,
-    mixed,
-    errors,
-]
+for name, restaurants in RESTAURANTS_WITH_NAMES.items():
+    # Sort to ensure always a nice diff
+    sorted_by_id = sorted(restaurants, key=lambda x: x["id"])
+    dest_data = {}
+    dest_data["elements"] = sorted_by_id
+    with open("./output/" + name + ".json", "w", encoding="utf-8") as f:
+        f.write(json.dumps(dest_data, ensure_ascii=False, indent=2))
 
-names = [
-    "t_only_vegan",
-    "t_only_vegetarian",
-    "t_vegetarian_with_vegan_options",
-    "t_some_veg_options",
-    "t_errors",
-]
-
-for restaurant_list, name in zip(sorted_restaurants, names):
-    new_data = {}
-    new_data["elements"] = restaurant_list
-    with open(name + ".json", "w", encoding="utf-8") as f:
-        f.write(json.dumps(new_data, ensure_ascii=False, indent=2))
+os.unlink("./output/source_data.json")
